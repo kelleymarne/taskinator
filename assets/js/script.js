c91d43c31cb37af6b3936a16c3dc1c51058beedc
Kelley Worthington <kelleymarne@gmail.com>
2021-05-12 21:43:16 -0600

    preventDefault function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // our function for creating a list item on click.
 var createTaskHandler = function () {
+
+    //clears out browser defaults to let the code run without refreshing
+    event.preventDefault();
+
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.textContent = "This is a new task!";
     tasksToDoEl.appendChild(listItemEl);
-}
+
+    
+};
 
 //calling the actual function
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
 
